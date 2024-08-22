48cae764d8e7514795d74350c8a7088ef8b6877c
ottonisd <jonathan.otto@nisd.net>
2024-08-22 11:02:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
-from datetime import datetime
-
 
 def main():
     if 'text_box' not in st.session_state:
-        st.session_state.text_box = st.text_input("Enter text:")
+        st.session_state.text_box = st.text_input("Enter text:", key="text_box_1")
     else:
-        st.session_state.text_box = st.text_input("Enter text:", value="")
+        st.session_state.text_box = st.text_input("Enter text:", value="", key="text_box_2")
 
     if st.button("Clear"):
-        st.session_state.text_box = st.text_input("Enter text:", value="")
+        st.session_state.text_box = st.text_input("Enter text:", value="", key="text_box_3")
 
 if __name__ == "__main__":
     main()
