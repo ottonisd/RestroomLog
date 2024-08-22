f87d3b253463d8f265cd6cc2649758d66ade7e2a
ottonisd <jonathan.otto@nisd.net>
2024-08-22 10:50:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ class TravelLogGUI:
 
 def main():
     app = TravelLogGUI()
+    id_text_value = ''
 
     st.title("Classroom Travel Log GUI")
 
-    id_text = st.text_input("Please scan ID and select.", key="id_text")
+    id_text = st.text_input("Please scan ID and select.", value=id_text_value, key="id_text")
     count_label = st.empty()
 
     if st.button("Restroom"):
@@ -34,10 +35,7 @@ def main():
                 app.count += 1
 
             count_label.write(f"Out to restroom: {app.count}")
-            # Clear the text box value
-            st.session_state.id_text = ''
-            # Create a new text input field with an empty value
-            st.text_input("Please scan ID and select.", value="", key="id_text")
+            id_text_value = ''
         else:
             st.write("Please scan ID and select.")
 
