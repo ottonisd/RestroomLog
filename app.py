68bff95bb1f8b2ddb358e274db6f0b598040cbe9
ottonisd <jonathan.otto@nisd.net>
2024-08-22 10:28:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ def main():
                 app.count += 1
 
             count_label.write(f"Out to restroom: {app.count}")
+            # Clear the ID text box
+            st.session_state.id_text = ""
         else:
             st.write("Please scan ID and select.")
 
