0ed2fee0bb177ec41081ae34962148fd7c90621f
ottonisd <jonathan.otto@nisd.net>
2024-08-22 11:17:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 
-if 'text_input_value' not in st.session_state:
-    st.session_state.text_input_value = ''
-
 placeholder = st.empty()
 
-input = placeholder.text_input('text', value=st.session_state.text_input_value, key=1)
+input = placeholder.text_input('text', key=1)
 click_clear = st.button('clear text input', key=3)
 if click_clear:
-    st.session_state.text_input_value = ''
-
-st.write(input)
+    placeholder.empty()
+    input = placeholder.text_input('text', value='', key=2)
