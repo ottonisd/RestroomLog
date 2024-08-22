d7d583c6b1923af4023bf6356ce0bea5bad3ac99
ottonisd <jonathan.otto@nisd.net>
2024-08-22 11:12:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ placeholder = st.empty()
 input = placeholder.text_input('text', key=1)
 click_clear = st.button('clear text input', key=3)
 if click_clear:
+    placeholder.empty()  # Clear the previous widget
     input = placeholder.text_input('text', value='', key=2)
 
 st.write(input)
