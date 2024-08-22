66ef1464758be3fb0172a773b2aaa7fd8db11bb7
ottonisd <jonathan.otto@nisd.net>
2024-08-22 10:57:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 import streamlit as st
 from datetime import datetime
 
-class TravelLogGUI:
-    def __init__(self):
-        self.count = 0
-        self.rr_list = []
-
-    def update_log(self, message, id_text):
-        with open("RRLog.txt", "a") as log_file:
-            log_file.write(f"{id_text}  {message}\n")
-        st.write(message)
-
-    def get_time(self):
-        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def main():
     if 'text_box' not in st.session_state:
