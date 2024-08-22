9178e7178a0f426969ce7cfd573bb329951a56ca
ottonisd <jonathan.otto@nisd.net>
2024-08-22 11:22:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 placeholder = st.empty()
 
-input = placeholder.text_input('text', key=1)
-click_clear = st.button('clear text input', key=3)
+input = placeholder.text_input('Enter a number:', key=1)
+click_clear = st.button('Clear', key=3)
 if click_clear:
+    message = f"{input} has left"
+    st.write(message)
     placeholder.empty()
-    input = placeholder.text_input('text', value='', key=2)
+    input = placeholder.text_input('Enter a number:', value='', key=2)
